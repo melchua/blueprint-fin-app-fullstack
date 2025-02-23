4a53eb47888e4c2301fb13c1ecc5f6e1a7f9035e
Mel Chua <mchuam@gmail.com>
2025-02-22 17:42:48 -0800

    Update database

--- backend/app/config.py
+++ backend/app/config.py
@@ -15,7 +15,10 @@ class Settings(BaseSettings):
     supabase_anon_key: str = Field(default=SUPABASE_ANON_KEY)
     supabase_db_password: str = Field(default=SUPABASE_DB_PASSWORD)
     supabase_connection_string: str = Field(
-        default=f"postgresql://postgres:{SUPABASE_DB_PASSWORD}@{SUPABASE_URL}:6543/postgres"
+        default=f"postgresql://postgres:{SUPABASE_DB_PASSWORD}@db.uozfntkdbijcqkgvpwnt.supabase.co:5432/postgres"
+    )
+    otherstring: str = Field(
+        default=f"postgresql://postgres.uozfntkdbijcqkgvpwnt:{SUPABASE_DB_PASSWORD}@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
     )
 
     class Config:
